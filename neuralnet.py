e1618ca0edf5d747972d91e3e35d7b61a7f70f7b
Takashi <taka4abe@gmail.com>
2020-06-04 21:48:27 +0900

    Update neuralnet.py

--- neuralnet.py
+++ neuralnet.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
-#Sorry! under constructoin!
+'''
+Sorry! under constructoin!
+
+Because my Ubuntu-18.04LTS PC automaticaly updated CUDA from version 10.1 to version 10.2, 
+I couldn't validate the keras/tf-gpu code by myself at this time.
+
+Since I'd like to check the code before push, please give me a time to fix it!
+Thank you,
+
+Takashi
